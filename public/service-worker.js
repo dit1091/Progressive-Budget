07dd06ca3067895798f420493921358997b2aa61
Dannette Tejeda <dannettetejeda@gmail.com>
2020-12-19 14:12:10 -0500

    database update

---
+++ public/service-worker.js
@@ -0,0 +1,53 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/indexedDb.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
+  
+  const CACHE_NAME = "static-cache-v1";
+  
+  // install
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+        });
+      })
+    );
+  });
